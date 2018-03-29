004e39e49d63b9de5630027179ad8e6b9e9d63be
Olga Shandrak <volha_shandrak@goodyear.com>
2018-03-29 13:32:34 +0300

    delete all components from foodCalc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {NavigationComponent} from "./navigation/navigation.component";
-import {MainContentComponent} from "./main-content/main-content.component";
-import {HomepageComponent} from "./main-content/homepage/homepage.component";
-import {InterestingComponent} from "./main-content/interesting/interesting.component";
-import {CalculatorComponent} from "./main-content/calculator/calculator.component";
-import {DishesListComponent} from "./main-content/dishes-list/dishes-list.component";
-import {AboutUsComponent} from "./main-content/about-us/about-us.component";
-import {HandbookComponent} from "./main-content/handbook/handbook.component";
-import {LoginComponent} from "./main-content/login/login.component";
-import {SignUpComponent} from "./main-content/sign-up/sign-up.component";
-import {UserService} from "./services/user.service";
-import {AuthenticationService} from "./services/authentication.service";
+import {NavigationComponent} from './navigation/navigation.component';
+import {MainContentComponent} from './main-content/main-content.component';
+import {HomepageComponent} from './main-content/homepage/homepage.component';
+import {AboutUsComponent} from './main-content/about-us/about-us.component';
+import {LoginComponent} from './main-content/login/login.component';
+import {SignUpComponent} from './main-content/sign-up/sign-up.component';
+import {UserService} from './services/user.service';
+import {AuthenticationService} from './services/authentication.service';
 import { EqualDirective } from './directives/equal.directive';
-import {ProfileComponent} from "./main-content/profile/profile.component";
-import {ProfileResolveComponent} from "./main-content/profile/profile.resolve.component";
-import {DatePipe} from "@angular/common";
+import {ProfileComponent} from './main-content/profile/profile.component';
+import {ProfileResolveComponent} from './main-content/profile/profile.resolve.component';
+import {DatePipe} from '@angular/common';
 
 const routes: Routes = [
     // basic routes
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: MainContentComponent },
-    { path: 'calculator', component: CalculatorComponent },
-    { path: 'dishes', component:  DishesListComponent },
-    { path: 'interesting', component:  InterestingComponent  },
-    { path: 'handbook', component:  HandbookComponent  },
     { path: 'about-us', component:  AboutUsComponent  },
     { path: 'sign-up', component:  SignUpComponent  },
     { path: 'login', component:  LoginComponent  },
@@ -50,10 +42,6 @@ const routes: Routes = [
       NavigationComponent,
       MainContentComponent,
       HomepageComponent,
-      CalculatorComponent,
-      DishesListComponent,
-      InterestingComponent,
-      HandbookComponent,
       AboutUsComponent,
       SignUpComponent,
       LoginComponent,
--- src/app/main-content/main-content.module.ts
+++ src/app/main-content/main-content.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainContentComponent } from './main-content.component';
-import {HomepageComponent} from "./homepage/homepage.component";
-import {InterestingComponent} from "./interesting/interesting.component";
-import {DishesListComponent} from "./dishes-list/dishes-list.component";
-import {CalculatorComponent} from "./calculator/calculator.component";
-import { HandbookComponent } from './handbook/handbook.component';
+import {HomepageComponent} from './homepage/homepage.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -15,6 +11,6 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     CommonModule
   ],
-  declarations: [MainContentComponent, HomepageComponent, InterestingComponent, CalculatorComponent, DishesListComponent, HandbookComponent, AboutUsComponent, LoginComponent, SignUpComponent, ProfileComponent]
+  declarations: [MainContentComponent, HomepageComponent, AboutUsComponent, LoginComponent, SignUpComponent, ProfileComponent]
 })
 export class MainContentModule { }
